f84fbfc415c11262d7d5d9c47318fc6f4a577469
eliranmal <eliranmal@gmail.com>
2019-03-07 01:55:23 +0200

    feat(framework): vue skeleton
    add initial vue template, switch runtime compilation on

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  lintOnSave: false,
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+};
