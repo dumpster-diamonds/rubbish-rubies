3f3393c278039525775dbaac6f43f56983acef3a
eliranmal <eliranmal@gmail.com>
2019-03-09 00:01:27 +0200

    feat(layout): layout / navigation
    extract partial templates as components, sort out layout and splash swapping

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer>
+    <ul>
+      <li v-for="item of menuItems">{{ item.title }}</li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      menuItems: [
+        {
+          title: 'wat'
+        },
+        {
+          title: 'foo'
+        },
+        {
+          title: 'bar'
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import "../styles/mixins";
+
+  footer {
+    text-align: right;
+    font-size: 1rem;
+    opacity: .555;
+
+    ul {
+      @include h-list;
+    }
+  }
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="map">
+    <input class="search"/>
+    <div class="paper"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+};
+</script>
+
+<style scoped lang="scss">
+  @import "../styles/extensions";
+
+  .map {
+    @extend %flex-stretch;
+
+    .search {
+      width: 100%;
+      height: 1.666rem;
+      line-height: 1.666rem;
+      padding: 0 1em;
+      margin: 0 0 1rem;
+      font-size: .666em;
+    }
+
+    .paper {
+      flex-grow: 1;
+      width: 100%;
+      height: 80%;
+      border: 1px dashed #aaa;
+    }
+  }
+</style>
