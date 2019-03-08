433309454d9b9fb31d6ded8aa049c75d0a92c301
eliranmal <eliranmal@gmail.com>
2019-03-08 02:17:37 +0200

    feat(styles): branding
    update app component layout and logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Rubbish Rubies"/>-->
+    <div class="rubies"></div>
+    <div class="logo">
+      <img alt="logo" src="./assets/logo.svg">
+    </div>
   </div>
 </template>
 
@@ -17,12 +20,41 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./styles/colors";
+  @import "./styles/mixins";
+
+
+  $ruby-colors: (
+    $color-fg-complement-1,
+    $color-fg-main,
+    lightyellow,
+    $color-fg-complement-2,
+    lightgreen,
+
+
+  );
+  $ruby-size: .777em;
+  $ruby-pad: 1.333em;
+
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding: 2em;
+    text-align: center;
+
+    @include rhombi('rubies', $ruby-colors, $ruby-size, $ruby-pad);
+
+    .logo {
+      img {
+        width: 30vw;
+        height: 30vw;
+        margin-left: -4vw; /* offset to keep the trash-bin centered */
+        margin-bottom: -8vw; /* give room for the rubies */
+      }
+    }
+  }
 </style>
