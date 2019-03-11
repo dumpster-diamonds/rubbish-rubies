7be9e9b0a04be70aec26555f9e419a25ce117c1e
eliranmal <eliranmal@gmail.com>
2019-03-11 22:28:42 +0200

    feat(components): map
    add fly-to transitions to the map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,7 +7,8 @@
     </places>
     <div class="paper">
       <MglMap v-bind="map"
-              :center.sync="center">
+              :center.once="initialLatLng"
+              @load="onMapLoad">
         <MglMarker v-bind="marker"
                    :coordinates.sync="markerCoordinates"/>
       </MglMap>
@@ -37,7 +38,7 @@
             appId: process.env.VUE_APP_ALGOLIA_APPLICATION_ID,
             apiKey: process.env.VUE_APP_ALGOLIA_API_KEY,
             options: {
-              countries: ['IL'],
+              countries: ['IL'], // todo - extract countries from env var?
             },
           },
         },
@@ -45,31 +46,43 @@
           accessToken: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN,
           mapStyle: process.env.VUE_APP_MAPBOX_MAP_STYLE_MINIMO,
           interactive: false,
-          zoom: 14,
+          zoom: 13.5, // todo - extract countries from env var?
         },
         marker: {
           // color: '#ff68d7',
           // color: '#c868ff',
           color: '#68b4ff',
         },
+        initialLatLng: {
+          lat: 32.0864,
+          lng: 34.7795,
+        },
       };
+    },
+    methods: {
+      async onMapLoad({ map, component }) {
+        this.mapboxMap = map;
+        this.mapboxAsyncActions = component.actions;
+      },
     },
     computed: {
       center: {
         get() {
           const { data: { latlng = {} } = {} } = this.search;
-          return Object.keys(latlng).length ? latlng : {
-            lat: 32.0864,
-            lng: 34.7795,
-          };
-        },
-        set(val) {
-
+          return Object.keys(latlng).length ? latlng : this.initialLatLng;
         },
       },
       markerCoordinates() {
         const { lat, lng } = this.center;
         return [lng, lat];
+      },
+    },
+    watch: {
+      async center(newVal, oldVal) {
+        await this.mapboxAsyncActions.flyTo({
+          center: newVal,
+          speed: 1
+        });
       },
     },
   };
